f383c1bb9d2401507386f1951bfe982b9e59bda1
Artur França Lezama <41313318+flartur95@users.noreply.github.com>
2018-09-13 16:37:57 -0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+	/*Faça um algoritmo que receba 3 notas e respectivos pesos, calcule e mostre a
+	média ponderada dessas notas. */
+int main(){
+
+	float nota1, nota2, nota3, mediaponderada; //declaradas variaveis float
+	int peso1 = 4; //variaveis inteiras
+	int peso2 = 4;
+	int peso3 = 2;
+	//explica pesos
+	printf("A primeira nota tem peso 4, a segunda prova tem peso 4 e a terceira tem peso 2.\n");
+
+	printf("Digite sua primeira nota: \n");
+	scanf("%f",&nota1);
+	printf("Digite sua segunda nota: \n");
+	scanf("%f",&nota2);
+	printf("Digite sua terceira nota: \n");
+	scanf("%f",&nota3);
+
+	// calculo da media ponderada
+	mediaponderada = ((nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3));
+	
+	printf("A media ponderada e: %.2f\n", mediaponderada);
+
+	system("pause");
+
+}
